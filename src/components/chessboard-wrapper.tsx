a36549962632f3e1849b2bf3b77320553003321e
Andrew Griffith <agriffi10@gmail.com>
2023-08-16 14:34:47 -0400

    adding base movability to the board

--- src/components/chessboard-wrapper.tsx
+++ src/components/chessboard-wrapper.tsx
@@ -1,5 +1,40 @@
 import Chessboard from "chessboardjsx";
+import { Chess, Move } from "chess.js";
+import { useEffect, useMemo, useState } from "react";
 
-export default function ChessBoardWrapper() {
-  return <Chessboard/>
+export interface IChessBoardWrapperProps {
+  position: string
+  showNotation: boolean
 }
+
+export interface IPlayerMove {
+  piece: string;
+  sourceSquare: string
+  targetSquare: string
+}
+
+const ChessBoardWrapper = () => {
+  const [game] = useState(new Chess())
+  const [position, setPosition] = useState(game.fen());
+  const [history, setHistory] = useState(game.history({verbose: true}));
+
+  
+  const onDrop = ({piece, sourceSquare, targetSquare}: IPlayerMove) => {
+    try{
+      game.move({from: sourceSquare, to: targetSquare});
+      setPosition(game.fen())
+      setHistory(game.history({verbose: true}))
+      console.log(history)
+    }
+    catch(e) {
+      console.warn(e)
+      console.log(piece)
+    }
+
+  }
+
+
+  return <Chessboard position={position}  onDrop={onDrop} />
+}
+
+export default ChessBoardWrapper
