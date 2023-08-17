2e75f9a37c049445813049ba1b5098b686d83f9e
Andrew G <30501888+agriffi10@users.noreply.github.com>
2023-08-17 16:49:15 -0400

    added in available moves highlight (#4)

--- src/components/chessboard-wrapper/chessboard-wrapper.tsx
+++ src/components/chessboard-wrapper/chessboard-wrapper.tsx
@@ -1,18 +1,31 @@
 import Chessboard from 'chessboardjsx';
-import { Chess, Move } from 'chess.js';
-import { useEffect, useMemo, useState } from 'react';
-import MoveHistory from '../game-history/game-history';
+import { Chess, Move, Square } from 'chess.js';
+import { useState } from 'react';
 import { ISimpleMove } from '../../interfaces/ISimpleMove';
 import Alert from '../alert/alert';
-import 'react-toastify/dist/ReactToastify.css';
-import './chessboard-wrapper.css';
+import MoveHistory from '../game-history/game-history';
 import GameOverScreen from '../game-over-screen/game-over-screen';
 import PlayerMenu from '../player-menu/player-menu';
+import 'react-toastify/dist/ReactToastify.css';
+import './chessboard-wrapper.css';
 
 interface IPlayerMove {
   piece: string;
   sourceSquare: string;
   targetSquare: string;
+}
+interface IHighlightStyle {
+  background: string;
+  borderRadius: string;
+}
+
+interface IHighlightStyleObject {
+  [key: string]: IHighlightStyle;
+}
+
+interface ISquareStyling {
+  pieceSquare: string;
+  history: Move[];
 }
 
 const ChessBoardWrapper = () => {
@@ -22,28 +35,30 @@ const ChessBoardWrapper = () => {
   const [currentPlayer, setCurrentPlayer] = useState('White');
   const [playerHistory, setPlayerHistory] = useState<ISimpleMove[]>([]);
   const [gameIsOver, setGameIsOver] = useState(false);
+  const [squareStyles, setSquareStyles] = useState<{}>({});
 
   const onDrop = ({ piece, sourceSquare, targetSquare }: IPlayerMove) => {
     if (sourceSquare === targetSquare) return;
+    // Making an illegal move was throwing an error, so we try/catch to handle each type of movement
     try {
       game.move({ from: sourceSquare, to: targetSquare, promotion: 'q' });
       setPosition(game.fen());
       checkCurrentPlayer();
       setMoves();
     } catch (e) {
-      // TODO: Alert player if actual invalid move or if in check
       checkPlayerState();
       checkIfGameOver();
     }
   };
 
   const setMoves = () => {
-    const history = game.history({ verbose: true });
-    console.log(history);
+    const history = getCurrentGameHistory();
+    // We want player history in reverse order for display purposes, newest always on top
     setPlayerHistory([
       ...(history.slice(-1) as ISimpleMove[]),
       ...playerHistory,
     ]);
+    removeHighlightSquare();
     checkIfGameOver();
   };
 
@@ -64,7 +79,6 @@ const ChessBoardWrapper = () => {
   };
 
   const checkIfGameOver = () => {
-    console.log(game.isGameOver());
     setGameIsOver(game.isGameOver());
   };
 
@@ -81,6 +95,45 @@ const ChessBoardWrapper = () => {
     setPlayerHistory([]);
   };
 
+  const getCurrentGameHistory = () => {
+    // Unaltered history according to the game
+    // Player history is an altered list
+    return game.history({ verbose: true });
+  };
+
+  const removeHighlightSquare = () => {
+    setSquareStyles({});
+  };
+
+  const highlightSquare = (
+    sourceSquare: string,
+    squaresToHighlight: string[]
+  ) => {
+    // List of current squares and available move squares
+    const squares = [sourceSquare, ...squaresToHighlight];
+    const highlightStyles: IHighlightStyleObject = {};
+    // Iterate and generate a style object for each of the squares
+    for (let i = 0; i < squares.length; i++) {
+      const key = squares[i];
+      highlightStyles[key] = {
+        background: 'radial-gradient(circle, #aec6cf 36%, transparent 40%)',
+        borderRadius: '50%',
+      };
+    }
+    setSquareStyles({ ...highlightStyles });
+  };
+
+  const onMouseOverSquare = (square: Square) => {
+    // Get list of moves' to locations and add highlights to them
+    const moves = game.moves({
+      square: square,
+      verbose: true,
+    });
+    if (moves.length === 0) return;
+    const squaresToHighlight = moves.map((move) => move.to);
+    highlightSquare(square, squaresToHighlight);
+  };
+
   return (
     <>
       <div id="play-area">
@@ -88,7 +141,13 @@ const ChessBoardWrapper = () => {
           <MoveHistory history={playerHistory} />
         </div>
         <div className="board">
-          <Chessboard position={position} onDrop={onDrop} />
+          <Chessboard
+            onMouseOverSquare={onMouseOverSquare}
+            position={position}
+            squareStyles={squareStyles}
+            onMouseOutSquare={removeHighlightSquare}
+            onDrop={onDrop}
+          />
         </div>
         <div className="menu">
           <PlayerMenu currentPlayer={currentPlayer} resetGame={resetGame} />
