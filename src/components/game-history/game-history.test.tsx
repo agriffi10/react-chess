f6cb380d6f44068a9610fbe20d61a2bf3480ea4e
Andrew Griffith <agriffi10@gmail.com>
2023-08-18 14:43:47 -0400

    more tests!

--- src/components/game-history/game-history.test.tsx
+++ src/components/game-history/game-history.test.tsx
@@ -3,23 +3,44 @@ import '@testing-library/jest-dom';
 import GameHistory from './game-history';
 import { ISimpleMove } from '../../interfaces/ISimpleMove';
 
-test('loads and displays greeting', () => {
-  const baseColor: ISimpleMove = {
-    color: 'w',
-    after: '',
-    before: '',
-    piece: 'p',
-    from: 'e7',
-    to: 'd7',
-    captured: 'q',
-  };
+describe('Game History', () => {
+  describe("Given I am playing a game of chess, when a move has been made, that move is displayed in the history tracker and attributed to the correct player'", () => {
+    test('White captures queen with pawn', () => {
+      const baseColor: ISimpleMove = {
+        color: 'w',
+        after: '',
+        before: '',
+        piece: 'p',
+        from: 'e7',
+        to: 'd7',
+        captured: 'q',
+      };
 
-  const history: ISimpleMove[] = [{ ...baseColor }];
-  const firstTestId = 'history-line-0';
-  render(<GameHistory history={history} />);
-  const lineItem = screen.getByTestId(firstTestId);
-  expect(lineItem).toBeInTheDocument();
-  expect(lineItem).toHaveTextContent(
-    'White moved Pawn from E7 to D7, captured Queen'
-  );
+      const history: ISimpleMove[] = [{ ...baseColor }];
+      const firstTestId = 'history-line-0';
+      render(<GameHistory history={history} />);
+      const lineItem = screen.getByTestId(firstTestId);
+      expect(lineItem).toBeInTheDocument();
+      expect(lineItem).toHaveTextContent(
+        'White moved Pawn from E7 to D7, captured Queen'
+      );
+    });
+    test('Black moves pawn', () => {
+      const baseColor: ISimpleMove = {
+        color: 'b',
+        after: '',
+        before: '',
+        piece: 'p',
+        from: 'e7',
+        to: 'd7',
+      };
+
+      const history: ISimpleMove[] = [{ ...baseColor }];
+      const firstTestId = 'history-line-0';
+      render(<GameHistory history={history} />);
+      const lineItem = screen.getByTestId(firstTestId);
+      expect(lineItem).toBeInTheDocument();
+      expect(lineItem).toHaveTextContent('Black moved Pawn from E7 to D7');
+    });
+  });
 });
