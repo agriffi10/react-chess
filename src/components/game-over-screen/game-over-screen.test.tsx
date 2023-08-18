b03e233fac2dde3466ac2cb2f628052647634bfa
Andrew Griffith <agriffi10@gmail.com>
2023-08-18 14:47:04 -0400

    scaffolding other tests

---
+++ src/components/game-over-screen/game-over-screen.test.tsx
@@ -0,0 +1,6 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+describe('Game Over Screen', () => {
+  describe('Given I am playing a game of chess, when the game has ended, then correct game over screen is displayed', () => {});
+});
---
+++ src/components/player-stats/player-stats.test.tsx
@@ -0,0 +1,6 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+describe('Player Stats', () => {
+  describe('Given I am a player, when a piece is taken and recorded in the move history, then my piece lost count is correct', () => {});
+});
