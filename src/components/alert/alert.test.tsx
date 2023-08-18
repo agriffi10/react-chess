a0ad3f3dd70486f63b1118363bf55f35a0f08094
Andrew Griffith <agriffi10@gmail.com>
2023-08-18 16:03:33 -0400

    starting work on alert test

---
+++ src/components/alert/alert.test.tsx
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+describe('Alert', () => {
+  describe('Given that I am playing the game, when I am in need of information, then the alert generates', () => {
+    test('Alert activates with message', () => {});
+  });
+});
--- src/components/chessboard-wrapper/chessboard-wrapper.test.tsx
+++ src/components/chessboard-wrapper/chessboard-wrapper.test.tsx
@@ -2,10 +2,10 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import ChessBoardWrapper from './chessboard-wrapper';
 
-describe('Game History', () => {
+describe('Chess Board Wrapper', () => {
   describe('Given that I have loaded the chess app, when I first start the game, then the game is in a default state', () => {
-    test('White captures queen with pawn', () => {
-      // Chessboard currently generates a warning because the component is now using deprecated properties
+    test('Everything loads without crashing', () => {
+      // Chessboard currently generates a warning because the library is now using deprecated properties
       render(<ChessBoardWrapper />);
       expect(screen.getByText('Total Moves - 0')).toBeInTheDocument();
       expect(screen.getByText("White's Move")).toBeInTheDocument();
