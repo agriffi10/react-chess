9a1011fbf87ebf38786293b1ad95fc4089a80fbb
Andrew Griffith <agriffi10@gmail.com>
2023-08-18 23:02:54 -0400

    Tests Wrapped

--- src/components/alert/alert.test.tsx
+++ src/components/alert/alert.test.tsx
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import Alert from './alert';
 
 describe('Alert', () => {
   describe('Given that I am playing the game, when I am in need of information, then the alert generates', () => {
-    test('Alert activates with message', () => {});
+    test('Alert activates with message', async () => {
+      const myAlert = 'Alert';
+      render(<Alert message={myAlert} clearMessageFromAlert={jest.fn()} />);
+      const alert = await screen.findByText(myAlert);
+      expect(alert).toBeInTheDocument();
+    });
   });
 });
